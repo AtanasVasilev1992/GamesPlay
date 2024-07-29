93a2abd571e1d88a484de1a74b879f8989028c9c
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-07-29 19:41:04 +0300

    Add hooks
    useForm hook and useGetAllGames hook

--- client/src/api/comments-api.js
+++ client/src/api/comments-api.js
@@ -3,7 +3,7 @@ import requester from "./requester";
 const BASE_URL = 'http://localhost:3030/jsonstore/games';
 const buildUrl = (gameId) => `${BASE_URL}/${gameId}/comments`;
 
-const create = async (gameId, username, text) => requester.post(buildUrl(gameId), {username, text});
+const create = async (gameId, username, text) => requester.post(buildUrl(gameId), { username, text });
 
 const getAll = async (gameId) => {
     const result = await requester.get(buildUrl(gameId));
