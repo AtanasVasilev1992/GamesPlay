5e1c4b7ee913d51b925a71bc2d6f42b63a5fa6d9
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-07-31 02:49:50 +0300

    Add dynamic navigation

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -7,9 +7,9 @@ export const useLogin = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-        const result = await login(email,password);
+        const {password: _ , ...authData} = await login(email,password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
         return result;
     };
@@ -21,11 +21,11 @@ export const useRegister = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await register(email, password);
+        const {password: _ , ...authData} = await register(email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
-        return result;
+        return authData;
     };
 
     return registerHandler;
